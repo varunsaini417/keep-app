68c60484c4ff6802d0dacb8b5bebc8e35c69e293
[varunsaini417] <[varunsaini417@gmail.com]>
2022-02-19 02:26:47 +0530

    added styles and added header in the app

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-
+import Header  from "./Header";
 function App(){
     return (
         <>
-            <h1>
-                Hello world
-            </h1>
+            <Header/>
         </>
     )
 }
